e609291d949736b0453f1681197ffab93130bde3
Кудрявцев Дмитрий Вальтерович <DVKudryavtsev@alfabank.ru>
2019-03-04 11:45:11 +0300

    Updates

---
+++ precache-manifest.3761c74e7efa0b238a9dad034d7dc266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "135f0cdfe56a044e8b1b",
+    "url": "/gamma-training/static/css/main.070a257a.chunk.css"
+  },
+  {
+    "revision": "135f0cdfe56a044e8b1b",
+    "url": "/gamma-training/static/js/main.135f0cdf.chunk.js"
+  },
+  {
+    "revision": "6b9e2c5a17feb0a32d69",
+    "url": "/gamma-training/static/js/runtime~main.6b9e2c5a.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/gamma-training/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gamma-training/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e1b66d12386574dc4fe73e120e4a7a52",
+    "url": "/gamma-training/index.html"
+  }
+];
